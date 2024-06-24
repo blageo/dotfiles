37cd6febd29150545819f4f5afb9bcb1e0214693
blageo <blageo@proton.me>
2024-06-24 16:03:51 -0400

    modularized debugging config

---
+++ .config/nvim/lua/custom/plugins/debugging.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+    },
+    config = function()
+      local dap, dapui = require 'dap', require 'dapui'
+
+      require('dapui').setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint, {})
+      vim.keymap.set('n', '<Leader>dc', dap.continue, {})
+
+      dap.adapters.cppdbg = {
+        id = 'cppdbg',
+        type = 'executable',
+        command = '/home/gbland/apps/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = 'Launch file',
+          type = 'cppdbg',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopAtEntry = true,
+        },
+      }
+
+      -- Use the same configuration for C
+      dap.configurations.c = dap.configurations.cpp
+    end,
+  },
+}
